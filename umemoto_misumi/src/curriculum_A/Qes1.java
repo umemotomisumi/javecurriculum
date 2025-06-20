9ded92dd28acfc5a10081993664bad8204a47e6e
sho19 <sho19@192.168.0.12>
2025-06-21 01:15:24 +0900

    7.7変数①課題完了

---
+++ umemoto_misumi/src/curriculum_A/Qes1.java
@@ -0,0 +1,28 @@
+package curriculum_A;
+
+public class Qes1 {
+
+	public static void main(String[] args) {
+		byte byteNum = 0;
+		short shortNum = 0;
+		int intNum = 0;
+		long longNum = 0L;
+		float floatNum = 0.0f;
+		double doubleNum = 0.0d;
+		char letter = '\u0000';
+		String letters = null;
+		boolean isBoolean = false;
+		
+		byteNum = 10;
+		shortNum =100;
+		intNum = 1000;
+		longNum = 10000;
+		floatNum = 9.5f;
+		doubleNum = 10.5;
+		letter = 'a';
+		letters = "ハロー";
+		isBoolean = true;
+
+	}
+
+}
