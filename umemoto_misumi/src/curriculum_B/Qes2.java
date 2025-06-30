bee7d69e936c306bc0cd895cc6401b12aeac5b41
sho19 <sho19@192.168.0.12>
2025-07-01 03:15:59 +0900

    課題を提出します。

--- umemoto_misumi/src/curriculum_B/Qes2.java
+++ umemoto_misumi/src/curriculum_B/Qes2.java
@@ -7,7 +7,7 @@ public class Qes2 {
 			System.out.println("合格です！");
 
 		//指定の条件分岐を実行しました。//
-        System.out.println();
+		System.out.println();
 		int age = 25;
 		if (age >= 20 && age <= 30) {
 			System.out.println("適正年齢です");
@@ -16,7 +16,7 @@ public class Qes2 {
 		}
 
 		//年齢が20歳以上30歳以下なら適正年齢ですと、それ以外ば対象外ですとコンソールに表示させます。//
-        System.out.println();
+		System.out.println();
 		age = 18;
 
 		if (age >= 20) {
@@ -28,7 +28,7 @@ public class Qes2 {
 		}
 
 		//20歳以上なら成人、13歳以上19歳未満ならティーンエイジャー、12歳以下なら子供とコンソールに表示させます。//
-		
+
 		System.out.println();
 		int x = 30;
 		int y = 15;
@@ -44,30 +44,30 @@ public class Qes2 {
 			max = z;
 		}
 		System.out.println(max);
-		
+
 		//一番大きい数値を判定してコンソールに表示させます。//
-		
+
 		System.out.println();
 		int num = 1;
 		if (num > 0) {
 			System.out.println("正の数です");
-		 } else if(num == 0) {
+		} else if (num == 0) {
 			System.out.println("0です");
-		 } else {
-			System.out.println("負の数です");
-		 }
-		
-		//numを1とし、大きいか小さいか同じかの結果をコンソールに表示させます。//
-		
-		System.out.println();
-		if (num %2== 0) {
-			System.out.println( "偶数です" );
 		} else {
-			System.out.println( "奇数です" );
+			System.out.println("負の数です");
 		}
-		
+
+		//numを1とし、大きいか小さいか同じかの結果をコンソールに表示させます。//
+
+		System.out.println();
+		if (num % 2 == 0) {
+			System.out.println("偶数です");
+		} else {
+			System.out.println("奇数です");
+		}
+
 		//numを1とし、偶数か奇数かの結果をコンソールに表示させます。//
-		
+
 	}
 
 }
