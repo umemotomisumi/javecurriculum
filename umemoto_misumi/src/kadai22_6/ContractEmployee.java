02c73a21e90e5b47cfd9850a197313eef960d595
sho19 <sho19@192.168.0.12>
2025-09-22 20:54:44 +0900

    課題を提出します。

--- umemoto_misumi/src/kadai22_6/ContractEmployee.java
+++ umemoto_misumi/src/kadai22_6/ContractEmployee.java
@@ -6,7 +6,7 @@ public class ContractEmployee extends Employee {
 	public ContractEmployee(String id, String name) {
 		super(id, name);
 	}
-	
+
 	@Override
 	public int costForDay(int hoursWorked) {
 		int hourlyRate = 1000;
